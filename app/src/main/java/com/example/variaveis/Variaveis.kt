6c03d87756406c3510e5441dcba9ae6303815ca2
VictorMG-Dev <139717201+VictorMG-Dev@users.noreply.github.com>
2025-03-05 17:12:38 -0300

    Teste Variaveis
    Curso Ebac

---
+++ app/src/main/java/com/example/variaveis/Variaveis.kt
@@ -0,0 +1,17 @@
+package com.example.variaveis
+
+class Variaveis {
+
+}
+
+fun main() {
+
+    //var Mutavel
+    var resultadoSoma: Int
+    var inteiro = 1 //"Int"
+
+    //val Imutavel
+    val resultado: String
+    val outroInteiro = "teste"
+
+}
