d7fd1fc490828bec625f5c6bf3869d1f1a7390b1
VictorMG-Dev <139717201+VictorMG-Dev@users.noreply.github.com>
2025-03-08 14:28:23 -0300

    Curso Ebac, treinamento com variaveis

---
+++ app/src/main/java/com/example/variaveis/Operations.kt
@@ -0,0 +1,34 @@
+package com.example.variaveis
+
+class Operations {
+}
+
+fun main() {
+
+    var teste = 1
+    var converstion:Double = teste.toDouble()
+
+    var a = 5
+    var b = 2
+    var resultado = a / b
+
+    println(resultado)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
